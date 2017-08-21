90d3c63d0002192313c253cc455a2515f6f8db3a
Web Dev <universalhobbyist@gmail.com>
2017-08-21 05:06:54 -0400

    pushing out updates for the letters guessed, countown for guesses remaining, current word replacing functioning, need to work on Wins and Prevent letters guessed 1: not be able to be resused (repressed) and clear the old letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,75 @@
 
-// function 
-
-
-
-//var userKey = event.key; 
+//Global Variables
 var randomWord;
 var wordList1 = ["squirtle", "charmander", "bulbasaur", "pikachu"];
 var userWins = 0; 
-var remainingGuesses = 0;
-
-//var randomWord = wordList1[Math.floor(Math.random() * wordList1.length)];
-
-//alert(randomWord);
-
-
-document.addEventListener("keyup", printKey);
+var remainingGuesses = 10; //come back later to do math based on how long the word is, give an additional 5 guesses
+var underscoredWord;
+var selectedWord;
+var Userkey;
 
 
-// Key Capture 
-function printKey(event) {
+// Capture the user's key 
+document.onkeyup = function(event) {
+	var userKey = event.key;
+	userKey = userKey.toLowerCase();
 
-	 userKey = event.key;
+    if(chosenWord.toLowerCase().indexOf(userKey.toLowerCase()) === -1){
+    
+        if(remainingGuesses == 0){
+           randomizer(wordList1);
+           remainingGuesses = 10;
+         }else{
+           remainingGuesses--;
+         }
+    }
 
-	alert("You pressed key: " + userKey);
+	//perform updates
+	$("#remainingGuesses").text(remainingGuesses);
+	$("#lettersGuessed").append(userKey+", ");
 
 
-	return userKey;
+	checkCharacter(userKey);
+	console.log(userKey)
 }
 
 
-function randomizer (array) {
+//Randomly picks a word out of the array that is passed to the function.
+ function randomizer (arr) {
 
-	myArray = array; 
+	randomGeneratedWord = Math.floor(Math.random() * (arr.length));
+	chosenWord = arr[randomGeneratedWord];
+	underscoredWord = chosenWord.replace(/./gi,"_");
 
-	randomSelectedWord = myArray[Math.floor(Math.random() * myArray.length)];
-
-	return randomSelectedWord;
+	console.log(chosenWord);
+	console.log(underscoredWord);
+	//return randomSelectedWord;
 }
 
 
-function blankCreator (randomWord) {
-
- blankString = ""; 
-
-	for (var i = 0; i < randomWord.length; i++) {
-
-		blankString += "_" + " ";
-	}
-
-	return blankString;
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substr(0,index) + chr + str.substr(index+1);
 }
 
+// Change character to selected one
+function checkCharacter(n) {
+	for(i = 0; i < chosenWord.length; i++){
+  		
+  		if(chosenWord[i].toLowerCase() == n.toLowerCase()){
 
+    	underscoredWord = setCharAt(underscoredWord,i,n);
 
+    	}
+  }
 
+  $("#currentWord").text(underscoredWord);
+  if(underscoredWord == chosenWord) {
+  	
+  	randomizer(wordList1);
+    remainingGuesses = 10;
+    $("#remainingGuesses").text(remainingGuesses);
+  }
+}
 
- randomWord = randomizer(wordList1); 
- blankWord = blankCreator(randomWord);
-
-alert(randomWord);
-alert(blankWord);
+randomizer(wordList1);
